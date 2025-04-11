93203f245ee125dabaeea691f018510a4366b45e
JOÃO VITOR GALIOTTO DE SOUZA <joao-vitor_g_souza@estudante.sesisenai.org.br>
2025-04-11 09:05:09 -0300

    feito testes e começo fundo

--- Classe.js
+++ Classe.js
@@ -8,20 +8,38 @@ class Game {
         this.ducks = ducks || [];
         this.image = image;
         this.bullets = bullets;
-        this.score = score;
+        this.score = score || 0;
         this.time = time;
         this.isGameOver = isGameOver;
-    }}
+    }
+
+    drawBackground(canva){
+        if (this.image) {
+            let img = new Image();
+            img.src = this.image;
+            canva.drawImage(img, this.x, this.y, this.w, this.h);
+        }
+    }
+    mov_background(speedX, speedY){
+        this.x += speedX;
+        this.y += speedY;
+    }
+
+    drawText(canva, text, x, y, font, color) {
+        canva.font = font;
+        canva.fillStyle = color;
+        canva.fillText(text, x, y);
+    }
+}
 
 class Ducks extends Game{
     speedX = 0;
     speedY = 0;
-    isShot = false;
 
-    duck_reset(){
-        if(this.x >= 1152){
-            this.y = Math.floor(Math.random() * 300)
-            this.x = -Math.floor(Math.random() * 201)
+    duck_reset(tiro){
+        if(this.x >= 1152 || tiro === true){
+            this.y = Math.floor(Math.random() * (1080 - this.h));
+            this.x = -Math.floor(Math.random() * 201);
         }
     }
 
@@ -63,7 +81,7 @@ class Scream extends Game{
 
     }
     faseScreenDraw(){
-
+    
     }
     homeScreenDraw(){
 
